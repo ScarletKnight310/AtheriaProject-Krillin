abe73b2746ef7fbb925e2dfb01d34c230663e1c0
Kashod Cagnolatti <kcagnolatti@callutheran.edu>
2019-11-05 23:02:00 -0800

    deleted DeityTag.java and replaced Gods.jav with NPCs.java / place majority of Base game info in the 'Core' class, gave base classes toString methods, removed getter from contentList class and gave each of its subclasses seperate getters

---
+++ Desktop/Projects/Java/AthProj_Krillin/src/content/NPCs.java
@@ -0,0 +1,49 @@
+package content;
+
+import java.util.ArrayList;
+import base.*;
+
+
+public class NPCs extends AthContentList
+{
+	ArrayList<ArrayList<AthObject>> npcs;
+	
+	public NPCs(AthObject[][] baseNpcs,AthObject[] deityDomain) 
+	{
+		super(deityDomain);
+		npcs = to2dArrayList(baseNpcs);
+	}
+	
+	//
+	public void addGodNPC(AthObject addon, AthObject godi)
+	{
+		content.add(godi);
+		npcs.get(0).add(addon);
+	}
+	
+	public void addNPC(AthObject addon)
+	{
+		npcs.get(1).add(addon);
+	}
+	
+	//
+	private AthObject getNPC(int t, int i)
+	{
+		return npcs.get(t).get(i);
+	}
+	
+	public AthObject getGodDomain(int i) 
+	{
+		return content.get(i);
+	}
+	
+	public AthObject getGodNPC(int i)
+	{
+		return getNPC(0,i);
+	}
+	
+	public AthObject getNPC(int i)
+	{
+		return getNPC(1,i);
+	}
+}
